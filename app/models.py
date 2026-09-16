7277b6735d03a810c053ab343344b5fe67bde6a2
Sujay Simha <bsujaysimha.cs26@rvce.edu.in>
2026-09-16 18:50:29 +0530

    fix: Change datatype of uid from int to str, remove UNIQUE constraint from usernames and subject codes

--- app/models.py
+++ app/models.py
@@ -3,12 +3,12 @@ from .exts import db, login_manager
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.String(5), primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     semesters = db.relationship('Semester', backref='user', lazy=True)
-    timetables = db.relationship('Timetable', backref='user', lazy=True)
+    subjects = db.relationship('Subject', backref='user', lazy=True)
 
 class Semester(db.Model):
     __tablename__ = 'semesters'
@@ -23,7 +23,7 @@ class Subject(db.Model):
     uid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     sid = db.Column(db.Integer, db.ForeignKey('semesters.id'), nullable=False)
     name = db.Column(db.String(80), nullable=False)
-    code = db.Column(db.String(20), unique=True, nullable=True)
+    code = db.Column(db.String(20), nullable=True)
     date_times = db.Column(db.JSON, nullable=True)
     attendance = db.Column(db.Integer, nullable=False, default=0)
     min_attendance = db.Column(db.Float, nullable=False, default=100.00)
